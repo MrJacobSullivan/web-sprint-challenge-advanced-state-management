1c65285bdc8adddc6814b65ea96f1509ab817805
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-05 11:40:42 -0500

    added structure for fetchSmurfs action

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,20 @@ const fetchStart = () => ({ type: ACTIONS.FETCH_START })
 const fetchSuccess = (result) => ({ type: ACTIONS.FETCH_SUCCESS, payload: result })
 const fetchError = (error) => ({ type: ACTIONS.FETCH_ERROR, payload: error })
 
-const fetchSmurfs = () => (dispatch) => {}
+const fetchSmurfs = () => (dispatch) => {
+  dispatch(fetchStart())
 
-export { ACTIONS }
+  axios
+    .get(url)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+export { ACTIONS, fetchSmurfs }
 
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
