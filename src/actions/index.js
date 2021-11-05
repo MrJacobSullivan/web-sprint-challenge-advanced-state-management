7e20f8fd9c9d9a3d5d8d9796c4465271ff0fb5b7
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-05 11:38:49 -0500

    added start, success, and error actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
+const url = 'http://localhost:3333/smurfs'
+
 const ACTIONS = {
   FETCH_START: 'FETCH_START',
   FETCH_SUCCESS: 'FETCH_SUCCESS',
   FETCH_ERROR: 'FETCH_ERROR',
 }
+
+const fetchStart = () => ({ type: ACTIONS.FETCH_START })
+const fetchSuccess = (result) => ({ type: ACTIONS.FETCH_SUCCESS, payload: result })
+const fetchError = (error) => ({ type: ACTIONS.FETCH_ERROR, payload: error })
+
+const fetchSmurfs = () => (dispatch) => {}
 
 export { ACTIONS }
 
