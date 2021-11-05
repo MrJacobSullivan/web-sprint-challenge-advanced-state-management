383d6d708f439935bb78beea45184d1d5dd4c7d3
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-11-05 11:50:20 -0500

    connected reducer to index.js using redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 
-import './index.css'
 import App from './App'
+import './index.css'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+
+import { reducer } from './reducers'
+
+const middleware = [logger, thunk]
+const store = createStore(reducer, applyMiddleware(...middleware))
 
 const { worker } = require('./mocks/browser')
 worker.start()
 
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(<App />, rootElement)
-
-//Task List:
-//1. Add in all necessary components and libary methods.
-//2. Create a store that includes thunk and logger middleware support.
-//3. Wrap the App component in a react-redux Provider element.
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+)
